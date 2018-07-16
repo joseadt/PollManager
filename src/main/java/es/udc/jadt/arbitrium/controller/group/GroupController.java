434052f8a10500da28e7035e090834dfcaca528e
Jose Antonio Dosil Toja <jose.dosilt@udc.es>
2018-07-16 16:24:52 +0200

    Añadir controlador para unirse a grupo #ARBI-45

--- src/main/java/es/udc/jadt/arbitrium/controller/group/GroupController.java
+++ src/main/java/es/udc/jadt/arbitrium/controller/group/GroupController.java
@@ -1,15 +1,21 @@
 package es.udc.jadt.arbitrium.controller.group;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 import es.udc.jadt.arbitrium.model.entities.group.UserGroup;
 import es.udc.jadt.arbitrium.model.service.group.GroupService;
 import es.udc.jadt.arbitrium.model.service.util.EntityNotFoundException;
+import es.udc.jadt.arbitrium.model.service.util.exceptions.UserAlreadyInGroupException;
 
 @Controller
 public class GroupController {
@@ -33,5 +39,18 @@ public class GroupController {
 		}
 
 		return GROUP_VIEW;
+	}
+
+	@PostMapping("group/{id}")
+	public String joinGroup(HttpServletRequest request, @PathVariable Long id) {
+		Principal principal = request.getUserPrincipal();
+
+		try {
+			this.groupService.joinGroup(id, principal.getName());
+		} catch (EntityNotFoundException | UserAlreadyInGroupException e) {
+			return "redirect:/error";
+		}
+
+		return "redirect:/group/" + id.toString();
 	}
 }
