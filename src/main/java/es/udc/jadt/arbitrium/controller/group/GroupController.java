93dd0935269a05776a684d0817514e0d27f24356
Jose Antonio Dosil Toja <jose.dosilt@udc.es>
2018-07-17 21:53:00 +0200

    Controlador de la interfaz #ARBI-50

--- src/main/java/es/udc/jadt/arbitrium/controller/group/GroupController.java
+++ src/main/java/es/udc/jadt/arbitrium/controller/group/GroupController.java
@@ -5,22 +5,27 @@ import java.security.Principal;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import es.udc.jadt.arbitrium.model.entities.group.UserGroup;
 import es.udc.jadt.arbitrium.model.service.group.GroupService;
 import es.udc.jadt.arbitrium.model.service.util.EntityNotFoundException;
 import es.udc.jadt.arbitrium.model.service.util.exceptions.UserAlreadyInGroupException;
+import es.udc.jadt.arbitrium.support.web.Ajax;
 
 @Controller
 public class GroupController {
 
 	private static final String GROUP_VIEW = "group/show";
+
+	private static final String SEARCH_GROUPS_VIEW = "group/search";
 
 	@Autowired
 	private GroupService groupService;
@@ -52,5 +57,24 @@ public class GroupController {
 		}
 
 		return "redirect:/group/" + id.toString();
+	}
+
+	@GetMapping("group/search")
+	public String searchGroups(Model model,
+			@RequestHeader(value = "X-Requested-With", required = false) String requestedWith,
+			@RequestParam(value = "keywords", required = false) String keywords,
+			@RequestParam(value = "page", required = false) Integer page) {
+		String returned = SEARCH_GROUPS_VIEW;
+		if (Ajax.isAjaxRequest(requestedWith)) {
+			returned = SEARCH_GROUPS_VIEW.concat(" :: groupListFragment");
+		}
+
+		Page<UserGroup> groups = this.groupService.searchGroups((page != null && page > 0) ? page : 0, keywords);
+
+		model.addAttribute("groupList", groups.getContent());
+		model.addAttribute("hasNext", groups.hasNext());
+		model.addAttribute("hasPrevious", groups.hasPrevious());
+
+		return returned;
 	}
 }
