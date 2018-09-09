7994e7e200b7d18a85973b601ee4c14608aa9f51
Jose Antonio Dosil Toja <jose.dosilt@udc.es>
2018-09-09 13:37:52 +0200

    #ARBI-80
    Cambio en comando de procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -jar target/dependency/webapp-runner.jar --port $PORT arbitrium-service/target/*.war
+web: java $JAVA_OPTS -jar  arbitrium-service/target/arbitrium.war -Dserver.port=$PORT
--- arbitrium-service/src/main/java/es/udc/jadt/arbitrium/config/ApplicationConfig.java
+++ arbitrium-service/src/main/java/es/udc/jadt/arbitrium/config/ApplicationConfig.java
@@ -5,14 +5,12 @@ import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
-@Import({ JPAConfiguration.class })
 class ApplicationConfig {
 
 	@Bean
